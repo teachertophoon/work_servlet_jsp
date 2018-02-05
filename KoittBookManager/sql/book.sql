f944088f606dfd56362b466e342ebaf8ad838602
exemplaryer <exemplaryer@gmail.com>
2018-02-05 11:29:27 +0900

    KoittBookManager - 1. 데이터베이스 bookmgr 만들기, 2. Book 테이블 만들기 (book.sql)

---
+++ KoittBookManager/sql/book.sql
@@ -0,0 +1,9 @@
+CREATE TABLE book(
+	isbn		INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	title		VARCHAR(30) NOT NULL,
+	author		VARCHAR(10) NOT NULL,
+	publisher	VARCHAR(30) NOT NULL,
+	price		INT NOT NULL,
+	description	VARCHAR(255) NULL 
+);
+
